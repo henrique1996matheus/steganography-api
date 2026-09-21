32523b2beedbebf50272023a3b939d3d6973dd9e
Matheus <matheus-henrique-1996@outlook.com>
2026-09-21 00:47:23 -0300

    Fix CORS

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers.analise_router import router as analise_router
 from routers.relatorio_router import router as relatorio_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(analise_router)
 app.include_router(relatorio_router)
